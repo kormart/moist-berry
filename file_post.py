55be04aae53ac1e5dac32bad00c2a08267f6487e
kormart <martin.korling@gmail.com>
2013-05-27 14:54:50 -0700

    file_post.py

---
+++ file_post.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+#import glob
+import time
+#from time import localtime
+#import httplib, urllib, socket
+import requests
+
+while 1:
+    try:
+        url = 'http://ec2-23-22-150-152.compute-1.amazonaws.com:3000/upload'
+        files = {'file': ('lastsnap.jpg', open('/tmp/motion/lastsnap.jpg', 'rb'))}
+        response = requests.post(url, files=files)
+#   print response.status_code, response.text
+    except (requests.ConnectionError) as ex:
+       print "Error: %s" % ex
+    time.sleep(60)
+
